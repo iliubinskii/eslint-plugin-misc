c7755d9a22bf8cf6e11591ddbce62064a667f778
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-24 14:56:04 +0300

    feat: Removed "no-empty-lines" rule

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -2,7 +2,6 @@ import { base } from "./base";
 import { componentName } from "./component-name";
 import { noComplexDeclaratorType } from "./no-complex-declarator-type";
 import { noComplexReturnType } from "./no-complex-return-type";
-import { noEmptyLines } from "./no-empty-lines";
 import { o } from "real-fns";
 
 export const vue = o.prefixKeys(
@@ -10,8 +9,7 @@ export const vue = o.prefixKeys(
     ...base,
     "component-name": componentName,
     "no-complex-declarator-type": noComplexDeclaratorType,
-    "no-complex-return-type": noComplexReturnType,
-    "no-empty-lines": noEmptyLines
+    "no-complex-return-type": noComplexReturnType
   },
   "vue/"
 );
