4b4ca651b5ace86fb7f0130938dc32628a582558
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-10-09 09:40:59 +0300

    fix(no-relative-parent-import): Fix for false negatives

--- tests/core/no-relative-parent-import.ts
+++ tests/core/no-relative-parent-import.ts
@@ -10,9 +10,19 @@ utils.testRule("no-relative-parent-import", rule, [
     name: `Test at line ${getCurrentLine().line}`,
     options: [{ allow: ["../allowed-source"] }],
     code: `
-      import x from "../source";
-      import y from "../allowed-source";
+      import x1 from "..";
+      import x2 from "../source";
+      import x5 from "../allowed-source";
+      import x3 from "../..";
+      import x4 from "../../source";
+      import x6 from "../../allowed-source";
     `,
-    errors: [{ line: 1, messageId: MessageId.disallowedSource }]
+    errors: [
+      { line: 1, messageId: MessageId.disallowedSource },
+      { line: 2, messageId: MessageId.disallowedSource },
+      { line: 4, messageId: MessageId.disallowedSource },
+      { line: 5, messageId: MessageId.disallowedSource },
+      { line: 6, messageId: MessageId.disallowedSource }
+    ]
   }
 ]);
