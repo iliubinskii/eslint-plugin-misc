610f1913c122961a6068d9c2c55c274b1f6b162d
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-01-31 19:14:00 +0200

    chore(eslint): Edited rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     "./node_modules/project-chore/eslint",
+    "./node_modules/project-chore/eslint/packages/typescript-misc",
     "./node_modules/project-chore/eslint/special-locations",
     "./.eslintrc.overrides",
     "./.eslintrc.rule-overrides",
